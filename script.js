cae13498879178eda028094e0ef433d952af734c
Singas <ravicmasilva@gmail.com>
2023-07-07 01:03:13 -0300

    done some responsive parts, started script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.querySelector('#year-result').innerHTML= '<span>22</span> anos'
+// document.querySelector('#month-result').innerHTML= '<span>3</span> meses'
+
+// mais ou menos a ideia pra manipular o campo de idades
